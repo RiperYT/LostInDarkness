0f6bd2be27a1addf26fdbca6a1348f1b31c1e633
riperyt <kutsenkoandrij2003@gmail.com>
2022-01-29 14:20:52 +0200

    Light dark

--- LID/Assets/Scripts/LevelUnder.cs
+++ LID/Assets/Scripts/LevelUnder.cs
@@ -8,29 +8,51 @@ public class LevelUnder : MonoBehaviour
     private bool First = false;
     private bool Second = false;
     private bool Third = false;
+
     public GameObject FirstDoor;
     public GameObject SecondDoor;
     public GameObject ThirdDoor;
 
+    public GameObject SecondDark;
+    public GameObject ThirdDark;
+
+    private void Start()
+    {
+        First = false;
+        FirstDoor.GetComponent<DoorOC>().CloseTheDoor();
+
+        Second = false;
+        SecondDoor.GetComponent<DoorOC>().CloseTheDoor();
+
+        Third = false;
+        ThirdDoor.GetComponent<DoorOC>().CloseTheDoor();
+
+        SecondDark.SetActive(true);
+        ThirdDark.SetActive(true);
+    }
+
     public void OpenFirst()
     {
-        if (FirstDoor == false)
+        if (First == false)
         {
             First = true;
             FirstDoor.GetComponent<DoorOC>().OpenTheDoor();
+            SecondDark.SetActive(false);
+
         }
     }
     public void OpenSecond()
     {
-        if (FirstDoor == false)
+        if (Second == false)
         {
             Second = true;
             SecondDoor.GetComponent<DoorOC>().OpenTheDoor();
+            ThirdDark.SetActive(false);
         }
     }
     public void OpenThird()
     {
-        if (FirstDoor == false)
+        if (Third == false)
         {
             Third = true;
             ThirdDoor.GetComponent<DoorOC>().OpenTheDoor();
