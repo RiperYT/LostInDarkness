b240f843388b59fa0a2c93d153eaa24c87e24e2a
riperyt <kutsenkoandrij2003@gmail.com>
2022-01-30 08:13:48 +0200

    Created and filled Upper level

---
+++ LID/Assets/Scripts/UpperDoor.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpperDoor : MonoBehaviour
+{
+    public GameObject door;
+    public GameObject emptyDoor;
+    public GameObject beetwen;
+    public GameObject dark;
+
+    private bool isOpen = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        emptyDoor.SetActive(true);
+        door.SetActive(false);
+        beetwen.SetActive(false);
+        dark.SetActive(true);
+        isOpen = false;
+    }
+
+    void FirstOpen()
+    {
+        if (!isOpen)
+        {
+            emptyDoor.SetActive(false);
+            door.SetActive(true);
+            beetwen.SetActive(true);
+            dark.SetActive(false);
+        }
+    }
+    void FirstClose()
+    {
+        if (isOpen)
+        {
+            emptyDoor.SetActive(true);
+            door.SetActive(false);
+            beetwen.SetActive(false);
+        }
+    }
+}
